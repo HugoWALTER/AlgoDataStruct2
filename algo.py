bba574dd0ca64d533d6847b0ff3483337381267c
HugoWALTER <hugo.walter@epitech.eu>
2020-03-26 01:46:41 +0100

    [FIX] - switch move to canvas.coords, create move func & refactor code to python norm

--- algo.py
+++ algo.py
@@ -10,7 +10,7 @@ class Vector:
         self.x = x
         self.y = y
 
-    def displayVector(self):
+    def display_vector(self):
         print(str(self.x)+';'+str(self.y))
 
 
@@ -22,149 +22,159 @@ class Window(tk.Frame):
         master.config(menu=menu)
 
         file_menu = tk.Menu(menu, tearoff=0)
-        file_menu.add_command(label="Load Map", command=self.openFileMap)
-        file_menu.add_command(label="Load Robot", command=self.openFileRobot)
+        file_menu.add_command(label="Load Map", command=self.open_file_map)
+        file_menu.add_command(label="Load Robot", command=self.open_file_robot)
         file_menu.add_command(label="Exit", command=self.quit)
         menu.add_cascade(label="File", menu=file_menu)
 
         self.canvas = tk.Canvas(self)
-        self.canvas.bind("<Button-1>", self.detectLeftClic)
-        self.canvas.bind("<Button-3>", self.detectRightClic)
+        self.canvas.bind("<Button-1>", self.detect_left_clic)
+        self.canvas.bind("<Button-3>", self.detect_right_clic)
         self.canvas.pack(fill=tk.BOTH, expand=True)
-        self.imageMap = None
-        self.imageRobot = None
-        self.renderMap = None
-        self.renderRobot = None
-        self.storedMap = None
-        self.storedRobot = None
-        self.StartCoordinate = None
-        self.GoalCoordinate = None
-        self.startPointDefined = False
-        self.goalPointDefined = False
-        self.gameStarted = False
-        self.chargeDefaultMap()
-        self.chargeDefaultRobot()
+        self.image_map = None
+        self.image_robot = None
+        self.render_map = None
+        self.render_robot = None
+        self.stored_map = None
+        self.stored_robot = None
+        self.start_coordinate = None
+        self.goal_coordinate = None
+        self.start_point_defined = False
+        self.goal_point_defined = False
+        self.game_started = False
+        self.charge_default_map()
+        self.charge_default_robot()
 
-    def resetGame(self):
-        self.imageMap = None
-        self.imageRobot = None
-        self.renderMap = None
-        self.renderRobot = None
-        self.storedMap = None
-        self.storedRobot = None
-        self.StartCoordinate = None
-        self.GoalCoordinate = None
-        self.startPointDefined = False
-        self.goalPointDefined = False
-        self.gameStarted = False
+    def reset_game(self):
+        self.image_map = None
+        self.image_robot = None
+        self.render_map = None
+        self.render_robot = None
+        self.stored_map = None
+        self.stored_robot = None
+        self.start_coordinate = None
+        self.goal_coordinate = None
+        self.start_point_defined = False
+        self.goal_point_defined = False
+        self.game_started = False
 
-    def chargeDefaultMap(self):
-        self.storedMap = Image.open("Room.bmp", "r")
-        self.storedMap = self.storedMap.resize((1280, 720), Image.ANTIALIAS)
-        w, h = self.storedMap.size
-        self.renderMap = ImageTk.PhotoImage(
-            self.storedMap)
+    def charge_default_map(self):
+        self.stored_map = Image.open("Room.bmp", "r")
+        self.stored_map = self.stored_map.resize((1280, 720), Image.ANTIALIAS)
+        w, h = self.stored_map.size
+        self.render_map = ImageTk.PhotoImage(
+            self.stored_map)
 
-        if self.imageMap is not None:
-            self.canvas.delete(self.imageMap)
+        if self.image_map is not None:
+            self.canvas.delete(self.image_map)
 
-        self.imageMap = self.canvas.create_image(
-            (w / 2, h / 2), image=self.renderMap)
+        self.image_map = self.canvas.create_image(
+            (w / 2, h / 2), image=self.render_map)
 
         root.geometry("%dx%d" % (w, h))
 
-    def chargeDefaultRobot(self):
-        self.storedRobot = Image.open("robot.bmp", "r")
-        self.storedRobot = self.storedRobot.resize((25, 25), Image.ANTIALIAS)
-        w, h = self.storedRobot.size
-        self.renderRobot = ImageTk.PhotoImage(
-            self.storedRobot)
-        if self.imageRobot is not None:
-            self.canvas.delete(self.imageRobot)
+    def charge_default_robot(self):
+        self.stored_robot = Image.open("robot.bmp", "r")
+        self.stored_robot = self.stored_robot.resize((25, 25), Image.ANTIALIAS)
+        w, h = self.stored_robot.size
+        self.render_robot = ImageTk.PhotoImage(
+            self.stored_robot)
+        if self.image_robot is not None:
+            self.canvas.delete(self.image_robot)
 
-    def openFileMap(self):
-        self.resetGame()
+    def open_file_map(self):
+        self.reset_game()
         filename = filedialog.askopenfilename(initialdir=os.getcwd(
         ), title="Select MAP File", filetypes=[("BMP Files", "*.bmp")])
         if not filename:
             return
 
-        self.storedMap = Image.open(filename)
-        self.storedMap = self.storedMap.resize((1280, 720), Image.ANTIALIAS)
-        w, h = self.storedMap.size
-        self.renderMap = ImageTk.PhotoImage(
-            self.storedMap)
+        self.stored_map = Image.open(filename)
+        self.stored_map = self.stored_map.resize((1280, 720), Image.ANTIALIAS)
+        w, h = self.stored_map.size
+        self.render_map = ImageTk.PhotoImage(
+            self.stored_map)
 
-        if self.imageMap is not None:
-            self.canvas.delete(self.imageMap)
+        if self.image_map is not None:
+            self.canvas.delete(self.image_map)
 
-        self.imageMap = self.canvas.create_image(
-            (w / 2, h / 2), image=self.renderMap)
+        self.image_map = self.canvas.create_image(
+            (w / 2, h / 2), image=self.render_map)
 
         root.geometry("%dx%d" % (w, h))
-        self.openFileRobot()
+        self.open_file_robot()
 
-    def openFileRobot(self):
-        if self.gameStarted == False:
+    def open_file_robot(self):
+        if self.game_started == False:
             filename = filedialog.askopenfilename(initialdir=os.getcwd(
             ), title="Select ROBOT File", filetypes=[("BMP Files", "*.bmp")])
             if not filename:
                 return
 
-            self.storedRobot = Image.open(filename)
-            self.storedRobot = self.storedRobot.resize(
+            self.stored_robot = Image.open(filename)
+            self.stored_robot = self.stored_robot.resize(
                 (25, 25), Image.ANTIALIAS)
-            w, h = self.storedRobot.size
-            self.renderRobot = ImageTk.PhotoImage(
-                self.storedRobot)
-            if self.imageRobot is not None:
-                self.canvas.delete(self.imageRobot)
+            w, h = self.stored_robot.size
+            self.render_robot = ImageTk.PhotoImage(
+                self.stored_robot)
+            if self.image_robot is not None:
+                self.canvas.delete(self.image_robot)
 
     def _create_circle(self, x, y, r, **kwargs):
         return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
     tk.Canvas.create_circle = _create_circle
 
-    def getColorPixelAtPos(self, x, y):
-        rgb_im = self.storedMap.convert('RGB')
+    def get_color_pixel_at_pos(self, x, y):
+        rgb_im = self.stored_map.convert('RGB')
         r, g, b = rgb_im.getpixel((x, y))
         print(r, g, b)
 
-    def detectLeftClic(self, event):
+    def detect_left_clic(self, event):
         print("left clicked at", event.x, event.y)
-        self.getColorPixelAtPos(event.x, event.y)
-        if self.renderMap is not None and self.renderRobot is not None and self.startPointDefined == False:
-            self.imageRobot = self.canvas.create_image(
-                (event.x, event.y), image=self.renderRobot)
-            self.StartCoordinate = Vector(event.x, event.y)
-            Vector.displayVector(self.StartCoordinate)
-            self.startPointDefined = True
+        self.get_color_pixel_at_pos(event.x, event.y)
+        if self.render_map is not None and self.render_robot is not None and self.start_point_defined == False:
+            self.image_robot = self.canvas.create_image(
+                (event.x, event.y), image=self.render_robot)
+            self.start_coordinate = Vector(event.x, event.y)
+            Vector.display_vector(self.start_coordinate)
+            self.start_point_defined = True
 
-    def detectRightClic(self, event):
+    def detect_right_clic(self, event):
         print("right clicked at", event.x, event.y)
-        if self.startPointDefined == True and self.goalPointDefined == False:
+        if self.start_point_defined == True and self.goal_point_defined == False:
             self.canvas.create_circle(
                 event.x, event.y, 10, fill="red", width=1)
-            self.GoalCoordinate = Vector(event.x, event.y)
-            Vector.displayVector(self.GoalCoordinate)
-            self.goalPointDefined = True
-            self.launchGame()
+            self.goal_coordinate = Vector(event.x, event.y)
+            Vector.display_vector(self.goal_coordinate)
+            self.goal_point_defined = True
+            self.launch_game()
 
-    def isWin(self):
-        if self.StartCoordinate.x == self.GoalCoordinate.x and self.StartCoordinate.y == self.GoalCoordinate.y:
+    def is_win(self, calc):
+        if calc.x == self.goal_coordinate.x and calc.y == self.goal_coordinate.y:
             print("ITS WIN")
             return True
         else:
             print("ITS LOOSE")
             return False
 
-    def launchGame(self):
+    def move(self):
+        Vector.display_vector(self.goal_coordinate)
+        calc = Vector(self.start_coordinate.x, self.start_coordinate.y)
+        calc.x = self.start_coordinate.x + 10
+        calc.y = self.start_coordinate.y + 0
+        self.canvas.coords(self.image_robot, calc.x, calc.y)
+        print("New pos X:", calc.x)
+        print("New pos Y:", calc.y)
+        # self.is_win(calc)
+        # self.canvas.move(self.image_robot, 10, 0)
+
+    def launch_game(self):
         print("Launch Game")
-        self.gameStarted = True
+        self.game_started = True
         print("Coord Start: ")
-        Vector.displayVector(self.StartCoordinate)
+        Vector.display_vector(self.start_coordinate)
         print("Coord Goal: ")
-        Vector.displayVector(self.GoalCoordinate)
-        self.canvas.move(self.imageRobot, 10, 0)
+        self.move()
 
         # time.sleep(1)
         # do simple loop to move start to goal pos (x & y)
